86e4433b3ae0a313fdf6bea577d1105d98367f76
Renan <renan.devnovato@gmail.com>
2024-09-30 21:15:55 -0300

    removendo x e y das funções testes

--- test/test.py
+++ test/test.py
@@ -3,15 +3,15 @@ from src.atp import adicionar, subtrair, multiplicar, dividir
 
 # funções com operações
 
-def test_adicionar(x, y):
+def test_adicionar():
     assert adicionar(5, 2) == 7
 
-def test_subtrair(x, y):
+def test_subtrair():
     assert subtrair(8, 7) == 1
 
-def test_multiplicar(x, y):
+def test_multiplicar():
     assert multiplicar(3, 2) == 6
 
-def test_dividir(x, y):
+def test_dividir():
     assert dividir(6, 3) == 2
     assert dividir(1, 0) == "Erro! Divisão por zero."
